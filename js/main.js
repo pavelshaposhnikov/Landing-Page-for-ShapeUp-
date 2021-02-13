66f609137c2d763f779f4962d9dfbd397add1e38
pavelshaposhnikov <77579244+pavelshaposhnikov@users.noreply.github.com>
2021-02-13 19:43:30 +0200

    add smooth scroll/sliders

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
+/* Mobile Nav */
+(function () {
+  var toggle = document.querySelector('.nav-toggle');
+        
+  toggle.addEventListener('click', function(e) {
+  this.classList.toggle('opened');
+  });
+})();
+         
+/* Nav Toggle */ 
+let nav = $("#nav");
+let navToggle = $("#navToggle");
+  
+$("#navToggle").on("click", function(event) {
+  event.preventDefault();
+  nav.toggleClass("show");
+});
+
+/* Smooth Scroll */
+$("[data-scroll]").on("click", function(event) {
+  event.preventDefault();
+      
+    let elementId = $(this).data(`scroll`);
+    let elementOffset = $(elementId).offset().top;
+              
+$("html, body").animate({
+  scrollTop: elementOffset - 0
+  }, 600);
+});
+
+/* Gallery Slider */
 $('.gallery__slider').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 8000,
   arrows: true
+});
+
+/* Reviews Slider */
+$('.reviews__inner').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
