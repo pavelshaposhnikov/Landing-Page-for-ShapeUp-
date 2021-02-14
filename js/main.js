6a3fbcd47ad2dc8c6783c8fd721a83a9ed5e4f49
pavelshaposhnikov <77579244+pavelshaposhnikov@users.noreply.github.com>
2021-02-14 21:35:35 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-/* Mobile Nav */
-(function () {
-  var toggle = document.querySelector('.nav-toggle');
-        
-  toggle.addEventListener('click', function(e) {
-  this.classList.toggle('opened');
-  });
-})();
-         
+
 /* Nav Toggle */ 
 let nav = $("#nav");
 let navToggle = $("#navToggle");
@@ -15,6 +7,21 @@ $("#navToggle").on("click", function(event) {
   event.preventDefault();
   nav.toggleClass("show");
 });
+
+
+$('.nav-toggle').on('click', function(event){
+  $('.nav-toggle').toggleClass('opened');
+  $('#body').toggleClass('no-scroll');
+});
+
+$('.nav__link').on('click', function(event){
+  $('#body').removeClass('no-scroll');
+  $('.nav').removeClass('show');
+  $('.nav-toggle').removeClass('opened');
+});
+
+
+
 
 /* Smooth Scroll */
 $("[data-scroll]").on("click", function(event) {
@@ -28,24 +35,67 @@ $("html, body").animate({
   }, 600);
 });
 
+
+
+
 /* Gallery Slider */
 $('.gallery__slider').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 8000,
-  arrows: true
+  arrows: true,
+    responsive: [
+      {
+        breakpoint: 1210,
+        settings: {
+            slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+            slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 370,
+        settings: {
+            slidesToShow: 1
+        }
+      }
+    ]
 });
 
+
+
+
+
 /* Reviews Slider */
-$('.reviews__inner').slick({
+$('.reviews__slider').slick({
   slidesToShow: 2,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
   dots: true,
   arrows: false,
+    responsive: [
+      {
+        breakpoint: 1190,
+        settings: {
+            slidesToShow: 1
+        }
+      }
+    ]
 }); 
+
+
+
+
 
 /* Scroll Up Button */
   
